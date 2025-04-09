05cfa18572cb85295d8b52675d1f6cf63a3a1979
Brandon-Corrales <brandon.nunez.corrales@est.una.ac.cr>
2025-04-08 23:41:19 -0600

    Commit Brandon

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,25 @@ function App() {
   const [todoList, setTodoList] = useState<Todo[]>([])
 
   const handleChange = (e: any) => {
-    setTodoDescription(e.target.value)
-  }
+    setTodoDescription(e.target.value);
+
+  } ;
 
   const handleClick = () => {
+    const tempTodoList = [...todoList];
+    const newTodo = { description: todoDescription }
 
-  }
+    tempTodoList.unshift(newTodo)
+  
+    setTodoList(tempTodoList)
+  } ;
+
 
   return (
     <div style={{ border: "1px solid red", padding: 10 }}>
       <div>
         <input
+        type ="text"
         value = {todoDescription}
         onChange={handleChange}
         style={{marginRight: 10}}/>
@@ -29,12 +37,23 @@ function App() {
       </div>
 
       <div>TODOs Here:</div>
+      <ul>
+      {todoList.map((todo, index) => {
+        return <li key ={index}>
+          <input type="checkbox"></input>
+           {todo.description}</li>
+      })}
+      </ul>
+      
     </div>
   );
-}
 
+}
 export default App;
 
-//1. post / save the Todo Item (in a native state)
+//1. post / save the Todo Item (in a native state) (create Itema, Read Item, Update Item, Delete Item)
 //2. Last item will be kept on top
-
+//3. Add a checkbox to each item
+//4. When the users clicks on the checkbox, the item will sink to the bottom of the list and 
+//5. When the users clicks on the checkbox, it will show the date the task was completed
+//6. Make sure when you refresh the page, the list is still there (use local storage) 
